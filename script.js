ff1c8ed54f0f262ab0b703ff967e3f9a80b9142e
Shahjehan Saeed <shahjehansaeed99@gmail.com>
2025-05-24 11:44:39 +0500

    Added dark/light mode toggle functionality

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function showMessage() {
   alert("Thanks for clicking the button!");
 }
+
+const toggleBtn = document.getElementById("modeToggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "Switch to Light Mode";
+  } else {
+    toggleBtn.textContent = "Switch to Dark Mode";
+  }
+});
